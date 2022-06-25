5efa1f6bf3832b09ecca9c09fa1941dc0a511185
Kevin Yang <kevin@include.ai>
2022-06-25 17:09:33 -0400

    add date

--- marketplace/dapp-starter/src/lib/time.ts
+++ marketplace/dapp-starter/src/lib/time.ts
@@ -7,3 +7,31 @@ export const timeConverter = (UNIX_timestamp) => {
   var time = `${month} ${date}, ${year}`;
   return time;
 }
+
+export const timeSince = (UNIX_timestamp) => {
+  const date = new Date(UNIX_timestamp * 1000);
+  const seconds = Math.floor(((new Date() as any) - (date as any)) / 1000);
+
+  let interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + ' years';
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + ' months';
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + ' days';
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + ' hours';
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + ' min';
+  }
+  return Math.floor(seconds) + 's';
+};
--- marketplace/dapp-starter/src/pages/sell/create.tsx
+++ marketplace/dapp-starter/src/pages/sell/create.tsx
@@ -6,25 +6,25 @@ import { useRouter } from 'next/router'
 import Communities from '@/components/Communities'
 import useCurrentUser from '@/hooks/useCurrentUser'
 
-import { readImageFromIPFS, uploadImageToIPFS } from "@/api/web3/ipfs";
-import { listItem } from "@/api/web3/contract";
+import { readImageFromIPFS, uploadImageToIPFS } from '@/api/web3/ipfs'
+import { listItem } from '@/api/web3/contract'
 
 export default function Create() {
 	const router = useRouter()
 	const currentUser = useCurrentUser()
 
-	const [title, setTitle] = useState<string>('');
-	const [price, setPrice] = useState<string>('');
-	const [description, setDescription] = useState<string>('');
-	const [imageSrc, setImageSrc] = useState<string>('');
+	const [title, setTitle] = useState<string>('')
+	const [price, setPrice] = useState<string>('')
+	const [description, setDescription] = useState<string>('')
+	const [imageSrc, setImageSrc] = useState<string>('')
 	const [imageSrcUrl, setImageSrcUrl] = useState<string>('')
-	const [listPublic, setListPublic] = useState(false);
+	const [listPublic, setListPublic] = useState(false)
 
-	const handleImageUpload = async (file) => {
-		const imageCID = await uploadImageToIPFS(file);
-		setImageSrc(imageCID);
-		const imageUrlIPFS = await readImageFromIPFS(imageCID);
-		setImageSrcUrl(imageUrlIPFS);
+	const handleImageUpload = async file => {
+		const imageCID = await uploadImageToIPFS(file)
+		setImageSrc(imageCID)
+		const imageUrlIPFS = await readImageFromIPFS(imageCID)
+		setImageSrcUrl(imageUrlIPFS)
 	}
 
 	const handleSubmit = async () => {
@@ -112,7 +112,7 @@ export default function Create() {
 												className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 											/>
 										</div>
-										<div>
+										<div className="col-span-6 sm:col-span-4">
 											<label htmlFor="about" className="block text-sm font-medium text-gray-700">
 												Description
 											</label>
@@ -128,70 +128,47 @@ export default function Create() {
 													}}
 												/>
 											</div>
-											<div>
-												<label
-													htmlFor="about"
-													className="block text-sm font-medium text-gray-700"
-												>
-													Description
-												</label>
-												<div className="mt-1">
-													<textarea
-														id="about"
-														name="about"
-														rows={3}
-														className="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-														value={description}
-														onChange={(event) => {setDescription(event.target.value)}}
-													/>
-												</div>
-											</div>
-											<div>
-												<label className="block text-sm font-medium text-gray-700">
-													Photos
-												</label>
-												<div className="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
-													<div className="space-y-1 text-center">
-														{ 
-															!imageSrcUrl && <svg
-																className="w-12 h-12 mx-auto text-gray-400"
-																stroke="currentColor"
-																fill="none"
-																viewBox="0 0 48 48"
-																aria-hidden="true"
-															>
-																<path
-																	d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-																	strokeWidth={2}
-																	strokeLinecap="round"
-																	strokeLinejoin="round"
-																/>
-															</svg>
-														}
-														<div className="flex text-sm text-gray-600">
-															<label
-																htmlFor="file-upload"
-																className="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-															>
-																<span>Upload a file</span>
-																<input
-																	id="file-upload"
-																	name="file-upload"
-																	type="file"
-																	className="sr-only"
-																	onChange={(e) => handleImageUpload(e.target.files[0])}
-																/>
-															</label>
-															<p className="pl-1">or drag and drop</p>
-														</div>
-														<p className="text-xs text-gray-500">
-															PNG, JPG, GIF up to 10MB
-														</p>
-														{imageSrcUrl && <img src={imageSrcUrl}></img>}
+										</div>
+										<div className="col-span-6 sm:col-span-4">
+											<label className="block text-sm font-medium text-gray-700">Photo</label>
+											<div className="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
+												<div className="space-y-1 text-center">
+													{!imageSrcUrl && (
+														<svg
+															className="w-12 h-12 mx-auto text-gray-400"
+															stroke="currentColor"
+															fill="none"
+															viewBox="0 0 48 48"
+															aria-hidden="true"
+														>
+															<path
+																d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+																strokeWidth={2}
+																strokeLinecap="round"
+																strokeLinejoin="round"
+															/>
+														</svg>
+													)}
+													<div className="flex text-sm text-gray-600">
+														<label
+															htmlFor="file-upload"
+															className="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+														>
+															<span>Upload a file</span>
+															<input
+																id="file-upload"
+																name="file-upload"
+																type="file"
+																className="sr-only"
+																onChange={e => handleImageUpload(e.target.files[0])}
+															/>
+														</label>
+														<p className="pl-1">or drag and drop</p>
 													</div>
 													<p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
 												</div>
 											</div>
+											<div className="mt-4">{imageSrcUrl && <img src={imageSrcUrl}></img>}</div>
 										</div>
 									</div>
 									<div className="px-4 py-5 space-y-6 bg-gray-50 sm:p-6">
