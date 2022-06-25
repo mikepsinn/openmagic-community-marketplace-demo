33cb8570f9f27ee968fa48f3c6ebe69171f6d756
Kevin Yang <kevin@include.ai>
2022-06-25 01:42:28 -0400

    link to profile

--- marketplace/dapp-starter/src/components/profile/MiniProfile.tsx
+++ marketplace/dapp-starter/src/components/profile/MiniProfile.tsx
@@ -2,8 +2,10 @@ import * as React from 'react'
 import ProfileImage from './ProfileImage'
 import { timeConverter } from '@/lib/time'
 import { ProfileType } from '@/api/walletScan'
+import { useRouter } from 'next/router'
 
 export default function MiniProfile({ profile }: { profile: ProfileType }) {
+	const router = useRouter();
 	return (
 		<div className="pb-2 mt-3 mb-4">
 			<div className="flex items-center space-x-5 ">
@@ -14,13 +16,18 @@ export default function MiniProfile({ profile }: { profile: ProfileType }) {
 					</div>
 				</div>
 				<div>
-					<h1 className="text-2xl font-bold text-gray-900">{profile.ensName || profile.address}
-					{profile.ensName && <span className='ml-2 text-sm font-normal text-gray-600'>{profile.address}</span>}
-					</h1>
+					<a className='cursor-pointer hover:underline' onClick={() => router.push(`/user/${profile.ensName || profile.address}`)}>
+						<h1 className="text-2xl font-bold text-gray-900">
+							{profile.ensName || profile.address}
+							{profile.ensName && (
+								<span className="ml-2 text-sm font-normal text-gray-600">{profile.address}</span>
+							)}
+						</h1>
+					</a>
 					{profile?.first_tx?.timestamp ? (
 						<p className="text-sm font-medium text-gray-500">
 							Web3 user since{' '}
-							<a href={`https://etherscan.io/tx/${profile.first_tx.hash}`} className="text-gray-900">
+							<a href={`https://etherscan.io/tx/${profile.first_tx.hash}`} className="text-gray-900 hover:underline">
 								<time dateTime="2020-08-25">{timeConverter(profile?.first_tx?.timestamp)}</time>
 							</a>{' '}
 						</p>
@@ -34,7 +41,7 @@ export default function MiniProfile({ profile }: { profile: ProfileType }) {
 				<p>Reviews: 124</p>
 				<p>DAOs: {profile.daos.totalDaos}</p>
 				<p>POAPs: {profile.poaps.length}</p>
-				<p>NFTs: {profile.nfts.ownedNfts.length==100 ? "100+" : profile.nfts.ownedNfts.length}</p>
+				<p>NFTs: {profile.nfts.ownedNfts.length == 100 ? '100+' : profile.nfts.ownedNfts.length}</p>
 				<p>Writings: {profile.mirror.length}</p>
 			</div>
 		</div>
