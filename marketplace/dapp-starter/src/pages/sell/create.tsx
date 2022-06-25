d16fbc6a76502acb18ae9f27eb9a3c83474817a4
Kevin Yang <kevin@include.ai>
2022-06-25 12:55:39 -0400

    install small

--- marketplace/dapp-starter/src/pages/sell/create.tsx
+++ marketplace/dapp-starter/src/pages/sell/create.tsx
@@ -41,7 +41,7 @@ export default function Create() {
 								</div>
 							</div>
 							<div className="mt-5 md:mt-0 md:col-span-2">
-								<form action="#" method="POST">
+								<form method="POST">
 									<div className="shadow sm:rounded-md sm:overflow-hidden">
 										<div className="px-4 py-5 space-y-6 bg-white sm:p-6">
 											<div className="col-span-6 sm:col-span-4">
