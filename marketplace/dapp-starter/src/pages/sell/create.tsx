64c1d0fe061efee9bcccb176033a0e94a05bcaf7
Kevin Yang <kevin@include.ai>
2022-06-25 16:41:23 -0400

    conditional check

--- marketplace/dapp-starter/src/pages/sell/create.tsx
+++ marketplace/dapp-starter/src/pages/sell/create.tsx
@@ -1,39 +1,41 @@
-import { useState } from "react";
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 
 import Communities from '@/components/Communities'
 import useCurrentUser from '@/hooks/useCurrentUser'
 
-import { uploadImageToIPFS } from "@/api/web3/ipfs";
-import { listItem } from "@/api/web3/contract";
+import { uploadImageToIPFS } from '@/api/web3/ipfs'
+import { listItem } from '@/api/web3/contract'
 
 export default function Create() {
-	const router = useRouter();
-	const currentUser = useCurrentUser();
+	const router = useRouter()
+	const currentUser = useCurrentUser()
 
-	const [title, setTitle] = useState<string>('');
-	const [price, setPrice] = useState<string>('');
-	const [description, setDescription] = useState<string>('');
-	const [imageSrc, setImageSrc] = useState<string>('');
+	const [title, setTitle] = useState<string>('')
+	const [price, setPrice] = useState<string>('')
+	const [description, setDescription] = useState<string>('')
+	const [imageSrc, setImageSrc] = useState<string>('')
 
-	const handleImageUpload = async (file) => {
-		const imageCID = await uploadImageToIPFS(file);
-		setImageSrc(imageCID);
+	const [listPublic, setListPublic] = useState(false)
+
+	const handleImageUpload = async file => {
+		const imageCID = await uploadImageToIPFS(file)
+		setImageSrc(imageCID)
 	}
 
 	const handleSubmit = async () => {
 		if (currentUser) {
-			const metadata = {		
+			const metadata = {
 				title,
 				price,
 				description,
 				imageSrc,
 				listPublicly: true,
 				communities: [],
-				timestamp: Math.floor(Date.now() / 1000)
-			};
+				timestamp: Math.floor(Date.now() / 1000),
+			}
 
-			await listItem(currentUser.address, metadata);
+			await listItem(currentUser.address, metadata)
 		}
 	}
 
@@ -67,145 +69,143 @@ export default function Create() {
 								<div className="px-4 sm:px-0">
 									<h3 className="text-lg font-medium leading-6 text-gray-900">Create Listing</h3>
 									<p className="mt-1 text-sm text-gray-600">
-                    Create a listing that everyone can see, or only people who are in your communities.
+										Create a listing that everyone can see, or only people who are in your
+										communities.
 									</p>
 								</div>
 							</div>
 							<div className="mt-5 md:mt-0 md:col-span-2">
 								{/* <form method="POST"> */}
-									<div className="shadow sm:rounded-md sm:overflow-hidden">
-										<div className="px-4 py-5 space-y-6 bg-white sm:p-6">
-											<div className="col-span-6 sm:col-span-4">
-												<label
-													htmlFor="title"
-													className="block text-sm font-medium text-gray-700"
-												>
-													Title
-												</label>
-												<input
-													type="text"
-													name="title"
-													id="title"
-													value={title}
-													onChange={(event) => {setTitle(event.target.value)}}
-													className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+								<div className="shadow sm:rounded-md sm:overflow-hidden">
+									<div className="px-4 py-5 space-y-6 bg-white sm:p-6">
+										<div className="col-span-6 sm:col-span-4">
+											<label htmlFor="title" className="block text-sm font-medium text-gray-700">
+												Title
+											</label>
+											<input
+												type="text"
+												name="title"
+												id="title"
+												value={title}
+												onChange={event => {
+													setTitle(event.target.value)
+												}}
+												className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											/>
+										</div>
+										<div className="col-span-6 sm:col-span-4">
+											<label htmlFor="price" className="block text-sm font-medium text-gray-700">
+												Price
+											</label>
+											<input
+												type="text"
+												name="price"
+												id="price"
+												value={price}
+												onChange={event => {
+													setPrice(event.target.value)
+												}}
+												className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											/>
+										</div>
+										<div>
+											<label htmlFor="about" className="block text-sm font-medium text-gray-700">
+												Description
+											</label>
+											<div className="mt-1">
+												<textarea
+													id="about"
+													name="about"
+													rows={3}
+													className="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+													value={description}
+													onChange={event => {
+														setDescription(event.target.value)
+													}}
 												/>
 											</div>
-											<div className="col-span-6 sm:col-span-4">
-												<label
-													htmlFor="price"
-													className="block text-sm font-medium text-gray-700"
-												>
-													Price
-												</label>
-												<input
-													type="text"
-													name="price"
-													id="price"
-													value={price}
-													onChange={(event) => {setPrice(event.target.value)}}
-													className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-												/>
-											</div>
-											<div>
-												<label
-													htmlFor="about"
-													className="block text-sm font-medium text-gray-700"
-												>
-													Description
-												</label>
-												<div className="mt-1">
-													<textarea
-														id="about"
-														name="about"
-														rows={3}
-														className="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-														value={description}
-														onChange={(event) => {setDescription(event.target.value)}}
-													/>
-												</div>
-											</div>
-											<div>
-												<label className="block text-sm font-medium text-gray-700">
-													Photos
-												</label>
-												<div className="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
-													<div className="space-y-1 text-center">
-														<svg
-															className="w-12 h-12 mx-auto text-gray-400"
-															stroke="currentColor"
-															fill="none"
-															viewBox="0 0 48 48"
-															aria-hidden="true"
+										</div>
+										<div>
+											<label className="block text-sm font-medium text-gray-700">Photos</label>
+											<div className="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
+												<div className="space-y-1 text-center">
+													<svg
+														className="w-12 h-12 mx-auto text-gray-400"
+														stroke="currentColor"
+														fill="none"
+														viewBox="0 0 48 48"
+														aria-hidden="true"
+													>
+														<path
+															d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+															strokeWidth={2}
+															strokeLinecap="round"
+															strokeLinejoin="round"
+														/>
+													</svg>
+													<div className="flex text-sm text-gray-600">
+														<label
+															htmlFor="file-upload"
+															className="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
 														>
-															<path
-																d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-																strokeWidth={2}
-																strokeLinecap="round"
-																strokeLinejoin="round"
+															<span>Upload a file</span>
+															<input
+																id="file-upload"
+																name="file-upload"
+																type="file"
+																className="sr-only"
+																onChange={e => handleImageUpload(e.target.files[0])}
 															/>
-														</svg>
-														<div className="flex text-sm text-gray-600">
-															<label
-																htmlFor="file-upload"
-																className="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-															>
-																<span>Upload a file</span>
-																<input
-																	id="file-upload"
-																	name="file-upload"
-																	type="file"
-																	className="sr-only"
-																	onChange={(e) => handleImageUpload(e.target.files[0])}
-																/>
-															</label>
-															<p className="pl-1">or drag and drop</p>
-														</div>
-														<p className="text-xs text-gray-500">
-															PNG, JPG, GIF up to 10MB
-														</p>
+														</label>
+														<p className="pl-1">or drag and drop</p>
 													</div>
+													<p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
 												</div>
 											</div>
 										</div>
-										<div className="px-4 py-5 space-y-6 bg-gray-50 sm:p-6">
-											<div>
-												<p className="block text-sm font-medium text-gray-700">List Publicly</p>
-												<div className="flex items-start mt-4">
-													<div className="flex items-center h-5">
-														<input
-															id="public"
-															name="public"
-															type="checkbox"
-															className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
-														/>
-													</div>
-													<div className="ml-3 text-sm">
-														<label htmlFor="public" className="font-medium text-gray-700">
-															Public Listing
-														</label>
-														<p className="text-gray-500">
-															Anyone can view and place an offer on your posting.
-														</p>
-													</div>
+									</div>
+									<div className="px-4 py-5 space-y-6 bg-gray-50 sm:p-6">
+										<div>
+											<p className="block text-sm font-medium text-gray-700">List Publicly</p>
+											<div className="flex items-start mt-4">
+												<div className="flex items-center h-5">
+													<input
+														id="public"
+														name="public"
+														type="checkbox"
+														checked={listPublic}
+														className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
+														onChange={() => setListPublic(!listPublic)}
+													/>
+												</div>
+												<div className="ml-3 text-sm">
+													<label htmlFor="public" className="font-medium text-gray-700">
+														Public Listing
+													</label>
+													<p className="text-gray-500">
+														Anyone can view and place an offer on your posting.
+													</p>
 												</div>
 											</div>
+										</div>
+										{!listPublic && (
 											<div>
 												<p className="block text-sm font-medium text-gray-700">
 													List to your Communities
 												</p>
 												{currentUser && <Communities profile={currentUser} />}
 											</div>
-										</div>
-										<div className="px-4 py-3 text-right bg-gray-50 sm:px-6">
-											<button
-												className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-black border border-transparent rounded-md shadow-sm hover:bg-gray-800 focus:outline-none"
-												onClick={handleSubmit}
-											>
-												Publish
-											</button>
-										</div>
+										)}
 									</div>
+									<div className="px-4 py-3 text-right bg-gray-50 sm:px-6">
+										<button
+											className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-black border border-transparent rounded-md shadow-sm hover:bg-gray-800 focus:outline-none"
+											onClick={handleSubmit}
+										>
+											Publish
+										</button>
+									</div>
+								</div>
 								{/* </form> */}
 							</div>
 						</div>
