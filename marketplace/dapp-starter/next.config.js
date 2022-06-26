920213d7eaf9e1005abf4d85469943b254bdc6fd
Kevin Yang <kevin@include.ai>
2022-06-26 01:48:37 -0400

    ignore ts

--- marketplace/dapp-starter/next.config.js
+++ marketplace/dapp-starter/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
